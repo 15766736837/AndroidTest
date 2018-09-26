7982cdedba36043b174125517b77964f48e535ba
Jason <805464248@qq.com>
2018-09-26 14:44:49 +0800

    添加了log

--- toolbardemo/src/main/java/com/lai/www/toolbardemo/ToolBarDemoActivity.java
+++ toolbardemo/src/main/java/com/lai/www/toolbardemo/ToolBarDemoActivity.java
@@ -3,6 +3,7 @@ package com.lai.www.toolbardemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.widget.Toast;
 
@@ -17,6 +18,8 @@ public class ToolBarDemoActivity extends AppCompatActivity {
 
         initViews();
         Toast.makeText(this, "修改了toast", Toast.LENGTH_SHORT).show();
+
+        Log.e("添加了log", "添加了log");
     }
 
     private void initViews() {
